0ba0bcefe070ce1c16841f54afba83b8c9963c41
sadanalshatti <70812089+sadanalshatti@users.noreply.github.com>
2020-10-11 15:23:47 +0300

    h

--- scripts.js
+++ scripts.js
@@ -8,6 +8,16 @@
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
 
+function word () {
+ document.getElementById("caption").innerHTML = 'القمر';
+document.getElementById('image').src = './images/moon.jpg'
+
+}
+  
+
+
+
+
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
@@ -17,8 +27,34 @@
     "يتغير الاسم أسفل الصورة ويصبح "القمر
 */
 
+
+let darkmode = false
+function mode (){
+darkmode = !darkmode
+
+ if (darkmode == false)
+  {
+        document.getElementById("caption").innerHTML = 'الشمس';
+        document.getElementById('image').src = './images/sun.jpg'
+    
+
+    }
+ 
+else {
+    document.getElementById("caption").innerHTML = 'القمر';
+    document.getElementById('image').src = './images/moon.jpg'
+
+}
+
+}
+
+
+
 /*  الوضع النهاري
     يتغير لون خلفية الصفحة للأبيض
     تتغير الصورة لصورة الشمس
     "يتغير الاسم أسفل الصورة ويصبح "الشمس
 */
+
+
+
